650fde1c7df91eec62b8227993a3976b74d627b8
haddongik <haddongik@gmail.com>
2025-02-16 02:33:18 +0900

    commit1

--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+@export var start:int
 @export var height:int
+@export var init_life:float = 100
+@export var life_decrease: float = 8  # 초당 감소량
 @export var pack_player:PackedScene
 @export var pack_tile:PackedScene
 
@@ -10,41 +13,46 @@ class floor_info:
 	
 	func _init(_pos:int,_floor:int):
 		pos = _pos
-		floor = _floor	
+		floor = _floor
 
-var player:Node2D
 var score:int
-var current_floor:floor_info
+var life:float
+var gameover:bool
+var player:Node2D
 var floor_queue:Array[floor_info]
 var floor_tiles:Array[Node2D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	score = 0
+	life = init_life
+	gameover = false
+	
 	_make_player()
 	_make_floors(height)
 	_make_tiles(height)
 	_update_tiles()
-	
-	score = 0
 	_update_score()
-	
-	current_floor = floor_queue.pop_front()
 	_update_player()
 	
 	$CanvasLayer/GameOver.visible = false
 	$CanvasLayer/Restart.visible = false
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	if gameover:
+		pass
+		
 	if Input.is_action_just_pressed("ui_left"):
 		_move_step(-1)
 	elif Input.is_action_just_pressed("ui_right"):
 		_move_step(1)
-
-func _is_out_of_screen(node: Node2D) -> bool:
-	var viewport_size = get_viewport_rect().size
-	var pos = node.global_position
-	return pos.x < 0 or pos.x > viewport_size.x or pos.y < 0 or pos.y > viewport_size.y
+	
+	life -= ( life_decrease * delta )
+	life = clamp(life, 0, 100)  # life 범위 제한
+	$CanvasLayer/ProgressBar.value = life
+	
+	if life <= 0:
+		_game_over()
 
 func _make_player():
 	player = pack_player.instantiate()
@@ -67,16 +75,13 @@ func _make_floors(count:int):
 		_make_floor()
 	
 func _move_step(next:int):
-	var next_floor = floor_queue.pop_front()
+	var cur_floor = floor_queue[start]
+	var next_floor = floor_queue[start+1]
 
-	print("your input", next)
-	print("expect input", next_floor.pos - current_floor.pos)
-	
-	if next_floor.pos - current_floor.pos == next:
-	#if true:
+	if next_floor.pos - cur_floor.pos == next:
 		print("correct")
-		current_floor = next_floor
 		score += 1
+		floor_queue.pop_front()
 		
 		_make_floor()
 		_update_tiles()
@@ -88,7 +93,7 @@ func _move_step(next:int):
 func _make_tiles(count:int):
 	for i in count:
 		var tile:Node2D = pack_tile.instantiate()
-		tile.global_position = Vector2(tile.global_position.x, 750 - (i * 80))
+		tile.global_position = Vector2(tile.global_position.x, 550 - (i * 50))
 		add_child(tile)
 		floor_tiles.append(tile)
 		
@@ -99,19 +104,20 @@ func _update_tiles():
 		floor_tiles[index].global_position = Vector2(new_x,floor_tiles[index].global_position.y)
 
 func _update_player():
-	player.global_position = floor_tiles[0].global_position
+	player.global_position = floor_tiles[start].global_position
 	player.global_position += Vector2(100,-50)
 	
 func _update_score():
-	$CanvasLayer/Score.text = "점수: %d" % score
+	$CanvasLayer/Score.text = "SCORE: %d" % score
 	
 func _game_over():
 	print("game over")
+	gameover = true
+	get_tree().paused = true
 	$CanvasLayer/GameOver.visible = true
 	$CanvasLayer/Restart.visible = true
 
 func clear_all():
-	current_floor = null
 	player.queue_free()
 	floor_queue.clear()
 	for tile in floor_tiles:
@@ -121,5 +127,7 @@ func clear_all():
 	floor_tiles.clear()  # 배열 비우기
 	
 func _on_restart_pressed() -> void:
+	gameover = false
+	get_tree().paused = false
 	clear_all()
 	_ready()
